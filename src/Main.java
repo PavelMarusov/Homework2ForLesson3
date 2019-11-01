3acdf58c87789ed04a33fa85babb93e6dadc5581
PavelMarusov <pavelmarusovs@gmail.com>
2019-11-01 17:48:16 +0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,28 @@
+public class Main {
+
+
+
+    public static void main(String[] args) {
+
+        String [] people = {"Pasha", "Sasha", "Masha"};
+
+        for (int i = 0; i < people.length ; i++) {
+            switch (i){
+                case 0:
+                    System.out.println("good morning, Pasha");
+                    break;
+                case 1:
+                    System.out.println("good day, Sasha");
+                    break;
+                case 2:
+                    System.out.println("good evening, Masha");
+                    break;
+
+
+        }
+
+
+        }
+
+    }
+}
