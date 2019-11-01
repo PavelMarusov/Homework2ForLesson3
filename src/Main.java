ffd97cbbeb12f3c59f3164c949cb312c18749ab5
PavelMarusov <pavelmarusovs@gmail.com>
2019-11-01 18:03:45 +0600

    addition

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,9 @@ public class Main {
     public static void main(String[] args) {
 
         String [] people = {"Pasha", "Sasha", "Masha"};
+        System.out.println(people[0]);
+        System.out.println(people[1]);
+        System.out.println(people[2]);
 
         for (int i = 0; i < people.length ; i++) {
             switch (i){
